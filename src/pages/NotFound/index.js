38b800d65710342e00d4048e8d400ac3c7e4b383
PTMAnymus <anhttt27@fpt.com.vn>
2022-11-20 16:52:37 +0700

    main

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
+    const navigate = useNavigate()
     return (
         <>
-            <p>NotFound</p>
+            <p onClick={navigate("")}>NotFound</p>
         </>
     )
 }
